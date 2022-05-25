0c3714783cd0d0a2635da4d2c10720ed682e7462
Stijn Mertens <stijn.mertens@expertum.net>
2022-05-25 21:42:47 +0200

    Removed usage of private class properties Cleanup of code Implemented the parent constructor

--- squads/BaseSquad.mjs
+++ squads/BaseSquad.mjs
@@ -6,9 +6,9 @@ import Hauler from '/user/creeps/Hauler';
 import SpawnQueue from '../SpawnQueue'
 
 class BaseSquad {
-    #id;
-    #members = [];
-    #lastMemberId = 0;
+    id;
+    members = [];
+    lastMemberId = 0;
 
     /**
      * Creates a squad of creeps
@@ -17,51 +17,27 @@ class BaseSquad {
      * @param {SpawnQueue} spawnQueue - Squad spawn location
      */
     constructor(id, roles, spawnQueue) {
-        // console.log("[D] request received to create a squad with roles: " + JSON.stringify(roles));
-        // this.#id = id;
-        // roles.forEach(role => {
-        //     this.#lastMemberId++;
+        console.log("[D] request received to create a squad with roles: " + JSON.stringify(roles));
+        this.id = id;
+        roles.forEach(role => {
+            this.lastMemberId++;
 
-        //     switch(role) {
-        //         case HAULER:
-        //             //const hauler = new Hauler(spawnQueue, this.#id, this.#lastMemberId);
-        //             const hauler = new Hauler(spawnQueue, this, this.#lastMemberId);
-        //             this.#members.push(hauler);
-        //             break;
-        //     }
-        // });
+            switch(role) {
+                case HAULER:
+                    //const hauler = new Hauler(spawnQueue, this.#id, this.#lastMemberId);
+                    const hauler = new Hauler(spawnQueue, this, this.lastMemberId);
+                    this.members.push(hauler);
+                    break;
+            }
+        });
     }
-
-// TODO: move spawning of creeps to spawn queue/manager
 
     // Methods
     /**
      * TODO
      */
      run() {
-        console.log("[D] Squad.run()");
-        console.log("[D] Squad members: " + JSON.stringify(this.#members));
-
-        // Verify if the squad is complete
-        // If not, spawn member
-
-        // Time for action
-        // for each member in the squad
-        this.#members.forEach(member => {
-            console.log("[D] Found member: " + JSON.stringify(member));
-            member.run();
-        });
-
-        // Spawn hauler
-        // if(!myHauler) {
-        //     myHauler = new Hauler(myMemory.mySpawn);
-        // }
-        // else {
-        //     console.log('[D] Hauler body: ' + JSON.stringify(myHauler.body));
-        //     console.log('[D] Hauler creep: ' + JSON.stringify(myHauler.creep));
-        //     myHauler.targetContainer = closeContainer;
-        //     myHauler.run();
-        // }
+        console.log("[W] Implement run() in the child class!");
      }
 
      #isSquadComplete() {
@@ -77,7 +53,7 @@ class BaseSquad {
          console.log("[D]  - memberId: " + memberId);
          console.log("[D]  - creep: " + creep);
 
-         this.#members[memberId-1].creep = creep;
+         this.members[memberId-1].creep = creep;
      }
 }
 
