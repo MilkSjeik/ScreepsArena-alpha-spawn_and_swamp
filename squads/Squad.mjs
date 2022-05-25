ec44f154ca4505111b738e94d9c6e95c5be1bd20
Stijn Mertens <stijn.mertens@expertum.net>
2022-05-25 21:40:10 +0200

    Removed usage of private class properties Cleanup code that is also present in parent class

--- squads/Squad.mjs
+++ squads/Squad.mjs
@@ -7,10 +7,6 @@ import Hauler from '/user/creeps/Hauler';
 import SpawnQueue from '../SpawnQueue'
 
 class Squad extends BaseSquad {
-    #id;
-    #members = [];
-    #lastMemberId = 0;
-
     /**
      * Creates a squad of creeps
      * @constructor
@@ -19,20 +15,6 @@ class Squad extends BaseSquad {
      */
     constructor(id, roles, spawnQueue) {
         super(id, roles, spawnQueue);
-
-        console.log("[D] request received to create a squad with roles: " + JSON.stringify(roles));
-        this.#id = id;
-        roles.forEach(role => {
-            this.#lastMemberId++;
-
-            switch (role) {
-                case HAULER:
-                    //const hauler = new Hauler(spawnQueue, this.#id, this.#lastMemberId);
-                    const hauler = new Hauler(spawnQueue, this, this.#lastMemberId);
-                    this.#members.push(hauler);
-                    break;
-            }
-        });
     }
 
     // Methods
@@ -41,14 +23,14 @@ class Squad extends BaseSquad {
      */
     run(memory) {
         console.log("[D] Squad.run()");
-        console.log("[D] Squad members: " + JSON.stringify(this.#members));
+        console.log("[D] Squad members: " + JSON.stringify(this.members));
 
         // Verify if the squad is complete
         // If not, spawn member
 
         // Time for action
         // for each member in the squad
-        this.#members.forEach(member => {
+        this.members.forEach(member => {
             console.log("[D] Found member: " + JSON.stringify(member));
             // TODO: If hauler: set target to retrieve energy
             //if (member.roles)
@@ -62,17 +44,6 @@ class Squad extends BaseSquad {
                 member.run();
             }
         });
-
-        // Spawn hauler
-        // if(!myHauler) {
-        //     myHauler = new Hauler(myMemory.mySpawn);
-        // }
-        // else {
-        //     console.log('[D] Hauler body: ' + JSON.stringify(myHauler.body));
-        //     console.log('[D] Hauler creep: ' + JSON.stringify(myHauler.creep));
-        //     myHauler.targetContainer = closeContainer;
-        //     myHauler.run();
-        // }
     }
 
     #isSquadComplete() {
@@ -81,14 +52,6 @@ class Squad extends BaseSquad {
 
     #spawnMember() {
         // TODO
-    }
-
-    updateMember(memberId, creep) {
-        console.log("[D] Squad.updateMember()");
-        console.log("[D]  - memberId: " + memberId);
-        console.log("[D]  - creep: " + creep);
-
-        this.#members[memberId - 1].creep = creep;
     }
 }
 
