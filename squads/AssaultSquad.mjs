e11bb13cb650d9078db02f0cbbf292fcfd5ac5dd
Stijn Mertens <stijn.mertens@expertum.net>
2022-05-25 22:23:43 +0200

    Initial implementation stub Assault squad

---
+++ squads/AssaultSquad.mjs
@@ -0,0 +1,58 @@
+'use strict'
+
+//import { Creep } from '/game/prototypes';
+import BaseSquad from './BaseSquad';
+import { HAULER } from '/user/constants';
+import Hauler from '/user/creeps/Hauler';
+import SpawnQueue from '../SpawnQueue'
+
+class AssaultSquad extends BaseSquad {
+    /**
+     * Creates a squad of creeps
+     * @constructor
+     * @param {Array} roles - An array of creep roles
+     * @param {SpawnQueue} spawnQueue - Squad spawn location
+     */
+    constructor(id, roles, spawnQueue) {
+        super(id, roles, spawnQueue);
+    }
+
+    // Methods
+    /**
+     * TODO
+     */
+    run(memory) {
+        console.log("[D] Squad.run()");
+        console.log("[D] Squad members: " + JSON.stringify(this.members));
+
+        // Verify if the squad is complete
+        // If not, spawn member
+
+        // Time for action
+        // for each member in the squad
+        this.members.forEach(member => {
+            console.log("[D] Found member: " + JSON.stringify(member));
+            // TODO: If hauler: set target to retrieve energy
+            //if (member.roles)
+            if (member.creep) {
+                const container = memory.getCloseContainer(member.creep);
+                console.log("[D] Setting container as source: " + JSON.stringify(container));
+                member.source = container;
+                console.log("[D] Set container as source: " + JSON.stringify(member.source));
+                member.target = memory.mySpawn;
+
+                member.run();
+            }
+        });
+    }
+
+    #isSquadComplete() {
+        // TODO
+    }
+
+    #spawnMember() {
+        // TODO
+    }
+}
+
+export default AssaultSquad;
